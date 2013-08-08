18ae1b69f90f0661e67fc913283472722f8a8612
lucasb-eyer <lucasb.eyer.be@gmail.com>
2013-08-08 19:20:29 +0200

    Fixed visitor leak (now the destructor is usable.)

--- src/main.cpp
+++ src/main.cpp
@@ -431,11 +431,11 @@ void dump(const char *file) {
 int main(int argc, char **argv) {
   if (argc <= 1) {
     printf("Usage: %s something.dem\n", argv[0]);
-    exit(1);
   } else {
     dump(argv[1]);
   }
 
+  delete visitor;
   return 0;
 }
 
