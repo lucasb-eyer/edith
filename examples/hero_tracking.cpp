dd75cea7ce06b13960bedc7a1d6f6c7f5495aa67
lucasb-eyer <lucasb.eyer.be@gmail.com>
2013-10-06 18:27:18 +0200

    Updated hero_tracking code, merged csv and json.

--- examples/hero_tracking.cpp
+++ examples/hero_tracking.cpp
@@ -1,34 +1,42 @@
-#ifndef _HERO_TRACKING_VISITOR_CSVS_H
-#define _HERO_TRACKING_VISITOR_CSVS_H
-
+#include <stdexcept>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include "debug.h"
 #include "property.h"
 #include "hero_player_helper.h"
 #include "visitor.h"
+#include "edith.h"
 
 // Prints out the heroe's positions at any given time.
-// Difference between this and the other one, is that this one's output is
-// exactly as needed by my python heatmap scripts
-class HeroTrackingVisitorCSVs : public Visitor {
+class HeroTrackingVisitor : public Visitor {
 public:
-    HeroTrackingVisitorCSVs(std::string basepath = "./")
-        : _basepath(basepath)
-        , _all((basepath + "all.csv").c_str())
+    HeroTrackingVisitor(bool json, std::string basepath = "./")
+        : _json(json)
+        , _basepath(basepath)
+        , _all((basepath + "all." + (_json ? "json" : "csv")).c_str())
     { }
 
-    virtual ~HeroTrackingVisitorCSVs()
+    virtual ~HeroTrackingVisitor()
     {
         std::ofstream heroes(_basepath + "heroes.txt");
 
         for(auto& kv : _files) {
             // Write out the hero names
             heroes << kv.first << std::endl;
+
+            if(_json) {
+                *kv.second << "]" << std::endl;
+            }
+
             // And close all the files.
             kv.second->flush();
             delete kv.second;
+        }
+
+        if(_json) {
+            _all << "]" << std::endl;
         }
     }
 
@@ -58,15 +66,38 @@ protected:
             int x = cell_x*128 + origin_prop->values[0];
             int y = cell_y*128 + origin_prop->values[1];
 
+            // Write the coords to the "coords of all heroes" file.
+            if(_json) {
+                if(_files.empty()) {
+                    _all << "[ ";
+                } else {
+                    _all << ", ";
+                }
+            }
+
+            // Write to the hero-specific files.
+            // TODO: A "hero created" callback would be nice, wouldn't it?
             std::string heroname(hero.clazz->name, 16);
             auto iFile = _files.find(heroname);
             if(iFile == _files.end()) {
-                _files[heroname] = new std::ofstream((_basepath + heroname + ".csv").c_str());
+                _files[heroname] = new std::ofstream((_basepath + heroname + (_json ? ".json" : ".csv")).c_str());
                 iFile = _files.find(heroname);
+
+                if(_json) {
+                    *(iFile->second) << "[ ";
+                }
+            } else if(_json) {
+                *(iFile->second) << ", ";
             }
 
-            *(iFile->second) << x << ',' << y << '\n';
-            _all             << x << ',' << y << '\n';
+            if(_json) {
+                *(iFile->second) << '[' << x << ',' << y << ']';
+                _all             << '[' << x << ',' << y << ']';
+            } else {
+                *(iFile->second) << x << ',' << y << '\n';
+                _all             << x << ',' << y << '\n';
+            }
+
         } catch(const std::out_of_range& e) {
             XASSERT(false, "%s", e.what());
         } catch(const std::bad_cast& e) {
@@ -77,9 +108,20 @@ protected:
 
 private:
     HeroPlayerHelper _hph;
+    bool _json;
     std::string _basepath;
     std::map<std::string, std::ofstream*> _files;
     std::ofstream _all;
 };
 
-#endif
+int main(int argc, char* argv[])
+{
+    if (argc <= 2) {
+        std::cerr << "Usage: " << argv[0] << " [csv|json] something.dem" << std::endl;
+        return 1;
+    }
+
+    HeroTrackingVisitor visitor(std::string(argv[1]) == "csv");
+    dump(argv[2], visitor);
+    return 0;
+}
